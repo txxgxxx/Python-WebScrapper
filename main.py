c4bbf5982d70f97035204e458f7aed82ea752841
txxgxxx <taegun1020@naver.com>
2024-01-10 18:06:03 +0900

    8.2 Render Template

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
 @app.route("/")
 def home():
-    return 'hey there!'
+    return render_template("home.html", name="taegeon")
+
+
+@app.route("/hello")
+def hello():
+    return render_template("home.html")
 
 app.run(port=4000, debug=True)
 
