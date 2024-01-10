cf912f31a6862796be6fc02295a12e175204aa73
txxgxxx <taegun1020@naver.com>
2024-01-10 19:06:32 +0900

    8.8 Cache

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from extractors.indeed import extract_indeed_jobs
 
 app = Flask("JobScrapper")
 
+
 @app.route("/")
 def home():
     return render_template("home.html", name="taegeon")
 
+db = {}
 
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    # indeed = extract_indeed_jobs(keyword)
-    wwr = extract_wwr_jobs(keyword)
-    jobs = wwr
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
 
 app.run(port=4000, debug=True)
